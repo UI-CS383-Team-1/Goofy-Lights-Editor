0359f90d7e04f090b9a923c61f53069df0f124df
Tim Clemans <timothy.d.clemans@gmail.com>
2017-03-24 16:19:17 -0700

    ui edits - picked some better widgets

--- build-Goofy-Lights-Editor-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
+++ build-Goofy-Lights-Editor-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -13,10 +13,16 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
@@ -25,25 +31,150 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QWidget *centralWidget;
+    QWidget *gridLayoutWidget;
+    QGridLayout *gridLayout;
+    QFrame *frame;
+    QTableView *tableView;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QLabel *label;
+    QComboBox *comboBox;
+    QLabel *label_3;
+    QWidget *gridLayoutWidget_2;
+    QGridLayout *gridLayout_2;
+    QWidget *gridLayoutWidget_3;
+    QGridLayout *gridLayout_3;
+    QWidget *gridLayoutWidget_4;
+    QGridLayout *gridLayout_4;
+    QWidget *gridLayoutWidget_5;
+    QGridLayout *gridLayout_5;
+    QWidget *gridLayoutWidget_6;
+    QGridLayout *gridLayout_6;
+    QLabel *label_5;
+    QWidget *gridLayoutWidget_7;
+    QGridLayout *gridLayout_7;
+    QLabel *label_2;
+    QWidget *gridLayoutWidget_8;
+    QGridLayout *gridLayout_8;
+    QLabel *label_4;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(710, 458);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayoutWidget = new QWidget(centralWidget);
+        gridLayoutWidget->setObjectName(QStringLiteral("gridLayoutWidget"));
+        gridLayoutWidget->setGeometry(QRect(140, 30, 321, 201));
+        gridLayout = new QGridLayout(gridLayoutWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        frame = new QFrame(gridLayoutWidget);
+        frame->setObjectName(QStringLiteral("frame"));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+
+        gridLayout->addWidget(frame, 0, 0, 1, 1);
+
+        tableView = new QTableView(centralWidget);
+        tableView->setObjectName(QStringLiteral("tableView"));
+        tableView->setGeometry(QRect(110, 260, 431, 61));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(100, 340, 80, 25));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(190, 340, 51, 25));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(20, 280, 59, 17));
+        comboBox = new QComboBox(centralWidget);
+        comboBox->setObjectName(QStringLiteral("comboBox"));
+        comboBox->setGeometry(QRect(260, 340, 51, 25));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(200, 10, 91, 17));
+        gridLayoutWidget_2 = new QWidget(centralWidget);
+        gridLayoutWidget_2->setObjectName(QStringLiteral("gridLayoutWidget_2"));
+        gridLayoutWidget_2->setGeometry(QRect(120, 270, 61, 41));
+        gridLayout_2 = new QGridLayout(gridLayoutWidget_2);
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setContentsMargins(11, 11, 11, 11);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        gridLayout_2->setContentsMargins(0, 0, 0, 0);
+        gridLayoutWidget_3 = new QWidget(centralWidget);
+        gridLayoutWidget_3->setObjectName(QStringLiteral("gridLayoutWidget_3"));
+        gridLayoutWidget_3->setGeometry(QRect(200, 270, 61, 41));
+        gridLayout_3 = new QGridLayout(gridLayoutWidget_3);
+        gridLayout_3->setSpacing(6);
+        gridLayout_3->setContentsMargins(11, 11, 11, 11);
+        gridLayout_3->setObjectName(QStringLiteral("gridLayout_3"));
+        gridLayout_3->setContentsMargins(0, 0, 0, 0);
+        gridLayoutWidget_4 = new QWidget(centralWidget);
+        gridLayoutWidget_4->setObjectName(QStringLiteral("gridLayoutWidget_4"));
+        gridLayoutWidget_4->setGeometry(QRect(280, 270, 61, 41));
+        gridLayout_4 = new QGridLayout(gridLayoutWidget_4);
+        gridLayout_4->setSpacing(6);
+        gridLayout_4->setContentsMargins(11, 11, 11, 11);
+        gridLayout_4->setObjectName(QStringLiteral("gridLayout_4"));
+        gridLayout_4->setContentsMargins(0, 0, 0, 0);
+        gridLayoutWidget_5 = new QWidget(centralWidget);
+        gridLayoutWidget_5->setObjectName(QStringLiteral("gridLayoutWidget_5"));
+        gridLayoutWidget_5->setGeometry(QRect(360, 270, 61, 41));
+        gridLayout_5 = new QGridLayout(gridLayoutWidget_5);
+        gridLayout_5->setSpacing(6);
+        gridLayout_5->setContentsMargins(11, 11, 11, 11);
+        gridLayout_5->setObjectName(QStringLiteral("gridLayout_5"));
+        gridLayout_5->setContentsMargins(0, 0, 0, 0);
+        gridLayoutWidget_6 = new QWidget(centralWidget);
+        gridLayoutWidget_6->setObjectName(QStringLiteral("gridLayoutWidget_6"));
+        gridLayoutWidget_6->setGeometry(QRect(430, 270, 61, 41));
+        gridLayout_6 = new QGridLayout(gridLayoutWidget_6);
+        gridLayout_6->setSpacing(6);
+        gridLayout_6->setContentsMargins(11, 11, 11, 11);
+        gridLayout_6->setObjectName(QStringLiteral("gridLayout_6"));
+        gridLayout_6->setContentsMargins(0, 0, 0, 0);
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(320, 340, 59, 17));
+        gridLayoutWidget_7 = new QWidget(centralWidget);
+        gridLayoutWidget_7->setObjectName(QStringLiteral("gridLayoutWidget_7"));
+        gridLayoutWidget_7->setGeometry(QRect(0, 20, 111, 231));
+        gridLayout_7 = new QGridLayout(gridLayoutWidget_7);
+        gridLayout_7->setSpacing(6);
+        gridLayout_7->setContentsMargins(11, 11, 11, 11);
+        gridLayout_7->setObjectName(QStringLiteral("gridLayout_7"));
+        gridLayout_7->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(0, 0, 59, 17));
+        gridLayoutWidget_8 = new QWidget(centralWidget);
+        gridLayoutWidget_8->setObjectName(QStringLiteral("gridLayoutWidget_8"));
+        gridLayoutWidget_8->setGeometry(QRect(520, 20, 111, 231));
+        gridLayout_8 = new QGridLayout(gridLayoutWidget_8);
+        gridLayout_8->setSpacing(6);
+        gridLayout_8->setContentsMargins(11, 11, 11, 11);
+        gridLayout_8->setObjectName(QStringLiteral("gridLayout_8"));
+        gridLayout_8->setContentsMargins(0, 0, 0, 0);
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(520, 0, 141, 17));
+        MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 710, 22));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
-        centralWidget = new QWidget(MainWindow);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        MainWindow->setCentralWidget(centralWidget);
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
@@ -56,6 +187,23 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "Play/Pause", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Stop", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "Timeline", Q_NULLPTR));
+        comboBox->clear();
+        comboBox->insertItems(0, QStringList()
+         << QApplication::translate("MainWindow", "0.25x", Q_NULLPTR)
+         << QApplication::translate("MainWindow", "0.5x", Q_NULLPTR)
+         << QApplication::translate("MainWindow", "0.75x", Q_NULLPTR)
+         << QApplication::translate("MainWindow", "1x", Q_NULLPTR)
+         << QApplication::translate("MainWindow", "1.25x", Q_NULLPTR)
+         << QApplication::translate("MainWindow", "1.5x", Q_NULLPTR)
+         << QApplication::translate("MainWindow", "1.75", Q_NULLPTR)
+        );
+        label_3->setText(QApplication::translate("MainWindow", "Main Grid", Q_NULLPTR));
+        label_5->setText(QApplication::translate("MainWindow", "speed", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "Colors", Q_NULLPTR));
+        label_4->setText(QApplication::translate("MainWindow", "Directions / Arrow keys", Q_NULLPTR));
     } // retranslateUi
 
 };
