22771447a1b4e19da20c87986471bfdce5550eed
JoshuaBonn1 <bonn7152@vandals.uidaho.edu>
2017-03-30 03:01:45 -0700

    Will work this time -.-

--- Goofy-Lights-Editor/mainwindow.cpp
+++ Goofy-Lights-Editor/mainwindow.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QApplication>
-#include <QPushButton>
+
+#include <string>
 #include <QFileDialog>
 #include <QDir>
 #include <QMessageBox>
+#include <QDebug>
+#include <QColorDialog>
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -12,11 +17,93 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->comboBox->setCurrentIndex(3);
 
+    //Clear main grid
+    state initFrame;
+    rgb blank;
+    blank.r = 0;
+    blank.g = 0;
+    blank.b = 0;
+    for(int r = 0; r < 10; r++){
+        for(int c = 0; c < 20; c++){
+            mainGrid[r][c] = blank;
+            initFrame.frame[r][c] = blank;
+        }
+    }
+
+    //Add initial state to animation
+    initFrame.time = 0;
+    animation.push_back(initFrame);
+
+    //Set current color to red
+    currentColor.setRgb(255, 0, 0);
+
+    //Build grid
+    QWidget *w = ui->GridWidget;
+    mainFrame = new QGridLayout;
+    QSizePolicy *policy = new QSizePolicy(QSizePolicy::Preferred,QSizePolicy::Preferred);
+    policy->setHeightForWidth(true);
+
+    /* creating grid size as 10*20 */
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 20; j++)
+        {
+            QPushButton *tmp = new QPushButton;
+
+            tmp->setSizePolicy(*policy);
+
+            connect(tmp, SIGNAL(clicked()), this, SLOT(assignColor()));
+            //connect(this, SIGNAL(clicked()), this, SLOT(assignColor(i, j)));
+
+            mainFrame->addWidget(tmp,i,j,1,1);
+        }
+    }
+
+    /*setting the color of the Buttons in the grid as gray*/
+    w->setStyleSheet("QPushButton {background-color: gray;"
+                     "border-style: outset;"
+                     "border-width: 1px;"
+                     "border-color: beige; }");
+
+    w->setLayout(mainFrame);
+
+    w->show();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::assignColor(){
+    //Get button pressed
+    QPushButton* pButton = qobject_cast<QPushButton*>(sender());
+
+    //Find X,Y location of button in mainFrame
+    int x, y, xs, xy;
+    int index = mainFrame->indexOf(pButton);
+    mainFrame->getItemPosition(index, &x, &y, &xs, &xy);
+    qDebug() << QString::number(x) << " " << QString::number(y);
+
+    //Update rgb value at X,Y in mainGrid
+    rgb color;
+    color.r = currentColor.red();
+    color.g = currentColor.green();
+    color.b = currentColor.blue();
+    mainGrid[x][y] = color;
+    //and current animation frame
+    animation[currentAnimation].frame[x][y] = color;
+
+    //Set color to current color
+    if (pButton) // this is the type we expect
+    {
+         if(currentColor.isValid()){
+             QString qss = QString("background-color: %1").arg(currentColor.name());
+             pButton->setStyleSheet(qss);
+         }
+    }
+
+
 }
 
 void MainWindow::on_pushButton_3_clicked()
@@ -40,6 +127,35 @@ void MainWindow::on_actionSave_As_triggered()
                         tr("Open File"),
                         QDir::homePath(),
                         "Goof Light Editor(*.gle)");
+
+    if(filename.isEmpty()) return;
+    else{
+        QFile file(filename);
+        if(!file.open(QIODevice::WriteOnly)){
+            QMessageBox::information(this, tr("Error"), file.errorString());
+            return;
+        }
+        else{
+            //Write file *phew*
+            for(int i = 0; i < animation.size(); i++){
+                file.write(std::to_string(animation[i].time).c_str());
+                file.write("\n");
+                for(int r = 0; r < 10; r++){
+                    for(int c = 0; c < 20; c++){
+                        //Write rgb into file
+                        file.write(std::to_string(animation[i].frame[r][c].r).c_str());
+                        file.write(" ");
+                        file.write(std::to_string(animation[i].frame[r][c].g).c_str());
+                        file.write(" ");
+                        file.write(std::to_string(animation[i].frame[r][c].b).c_str());
+                        file.write(" ");
+                    }
+                    file.write("\n");
+                }
+            }
+        }
+    }
+
     QMessageBox::information(this,tr("File Name"),filename);
 
 }
@@ -63,4 +179,58 @@ void MainWindow::on_actionExport_triggered()
     QMessageBox::information(this,tr("File Name"),filename);
 
 
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    QMessageBox::information(this, "Info", "Hello");
+    for(int i = 0; i < animation.size(); i++){
+        std::cout << "Time stamp: " << animation[i].time << std::endl;
+        for(int r = 0; r < 10; r++){
+            for(int c = 0; c < 20; c++){
+                std::cout << animation[i].frame[r][c].r << " " << animation[i].frame[r][c].g << " " << animation[i].frame[r][c].b << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+void MainWindow::on_AddFrameButton_clicked()
+{
+    state temp;
+    temp.time = animation.size() * 5;
+    for(int r = 0; r < 10; r++){
+        for(int c = 0; c < 20; c++){
+            temp.frame[r][c] = mainGrid[r][c];
+        }
+    }
+
+    animation.push_back(temp);
+    currentAnimation++;
+}
+
+
+
+void MainWindow::on_DeleteFrameButton_clicked()
+{
+    if(animation.size() > 1){
+        //Remove last state
+        animation.pop_back();
+        currentAnimation--;
+
+        //Update mainGrid to current animation state
+        for(int r = 0; r < 10; r++){
+            for(int c = 0; c < 20; c++){
+                mainGrid[r][c] = animation[currentAnimation].frame[r][c];
+                QColor *color = new QColor(animation[currentAnimation].frame[r][c].r, animation[currentAnimation].frame[r][c].g, animation[currentAnimation].frame[r][c].b);
+                QString qss = QString("background-color: %1").arg(color->name());
+                mainFrame->itemAtPosition(r, c)->widget()->setStyleSheet(qss);
+            }
+        }
+    }
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    currentColor = QColorDialog::getColor(Qt::white, this);
 }
